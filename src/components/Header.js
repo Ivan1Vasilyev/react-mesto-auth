80dde0cc66a6178c2346f9d66cba95f612518c67
Ivan Vasilyev <Nalivan3747@yandex.ru>
2022-11-08 21:23:02 +0300

    feat/бургер готов

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,27 @@
+import { useState } from 'react';
 import logo from '../images/logo.svg';
 import Navbar from './Navbar';
 
-const Header = props => (
-  <header className="header">
-    <img className="header__logo" src={logo} alt="Логотип" />
-    <Navbar {...props} />
-  </header>
-);
+const Header = props => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const toggler = () => setIsMenuOpen(!isMenuOpen);
+
+  return (
+    <>
+      {isMenuOpen && props.loggedIn && <Navbar {...props} isOpen={isMenuOpen} />}
+      <header className="header">
+        <img className="header__logo" src={logo} alt="Логотип" />
+        <Navbar {...props} />
+        {props.loggedIn && (
+          <button className={`menu-trigger ${isMenuOpen && 'active'}`} onClick={toggler}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </button>
+        )}
+      </header>
+    </>
+  );
+};
 
 export default Header;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,16 @@
 import { Switch, Route, Link } from 'react-router-dom';
 
-const Navbar = ({ email, loggedIn, onSignOut }) => (
-  <div className="navbar">
+const Navbar = ({ email, loggedIn, onSignOut, isOpen }) => (
+  <div className={`navbar ${loggedIn && (isOpen ? 'navbar_state_showed' : 'navbar_state_hidden')}`}>
     <p className="navbar__email">{loggedIn && email}</p>
     <Switch>
       <Route path="/signin">
-        <Link to="signup" className="navbar__button">
+        <Link to="signup" className={`navbar__button ${loggedIn || 'navbar__button_showed'}`}>
           Регистрация
         </Link>
       </Route>
       <Route path="/signup">
-        <Link to="signin" className="navbar__button">
+        <Link to="signin" className={`navbar__button ${loggedIn || 'navbar__button_showed'}`}>
           Войти
         </Link>
       </Route>
