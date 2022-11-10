00392cc1c3cb3105944ebe42156e3c92880e3594
Ivan Vasilyev <Nalivan3747@yandex.ru>
2022-11-10 18:33:47 +0300

    try on mobile

--- src/components/App.js
+++ src/components/App.js
@@ -204,7 +204,7 @@ const App = () => {
         <Header email={email} loggedIn={loggedIn} onSignOut={onSignOut} />
         <Switch>
           <ProtectedRoute
-            component={Main}
+            component={PageNotFoundgit}
             loggedIn={loggedIn}
             path="/main"
             onEditAvatar={openEditAvatarPopup}
--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -1,7 +1,9 @@
 import { Link } from 'react-router-dom';
+import image from '../images/not-found.jpg';
 
 const PageNotFound = () => (
   <div className="not-found">
+    <img className="not-found__image" src={image} alt="Страница не найдена" />
     <Link className="not-found__link" to="/">
       ← Назад
     </Link>
