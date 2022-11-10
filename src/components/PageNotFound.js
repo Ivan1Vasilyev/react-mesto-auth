9a4ea5d5a19c0fef397c41f496ace7ca5eb3c3ca
Ivan Vasilyev <Nalivan3747@yandex.ru>
2022-11-10 18:56:25 +0300

    fix mobile 404

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -1,9 +1,13 @@
 import { Link } from 'react-router-dom';
 import image from '../images/not-found.jpg';
+import littleImage from '../images/not-found-mobile.jpg';
 
 const PageNotFound = () => (
   <div className="not-found">
-    <img className="not-found__image" src={image} alt="Страница не найдена" />
+    <picture>
+      <source srcSet={image} media="(min-width: 590px)" />
+      <img className="not-found__image" src={littleImage} alt="Страница не найдена" />
+    </picture>
     <Link className="not-found__link" to="/">
       ← Назад
     </Link>
